2f3491e71ef9d93448e64d66ac65b41b3a0269c2
arkadiuszklapa <sirarek@gmail.com>
2024-11-14 00:22:52 +0100

    feat: add translation

--- src/components/SaveAsScreenshotButton.jsx
+++ src/components/SaveAsScreenshotButton.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {useThree} from "@react-three/fiber";
 import {Html} from "@react-three/drei";
 import {Button} from "@mui/material";
-
 import {useTranslation} from "react-i18next";
 
 function SaveAsScreenshotButton(props) {
